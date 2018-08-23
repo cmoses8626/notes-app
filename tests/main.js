a49c64c27f37657c647d77606edd001206ea4f5f
Chris Moses <cmosesatmit@gmail.com>
2018-08-23 14:30:35 -0400

    Renamed files to use app-boilerplate to match git remote

--- tests/main.js
+++ tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("Notes app", function () {
+describe("App Boilerplate", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "notes-app");
+    assert.strictEqual(name, "app-boilerplate");
   });
 
   if (Meteor.isClient) {
